c7d1279e3976e7d5cc78d34b948412621f1a7b61
wahyu874 <79302418+wahyu874@users.noreply.github.com>
2021-04-02 15:47:19 +0800

    Tugas3
    Gede Wahyu Wastuguna 1915091042 SI4B

--- lib/beranda.dart
+++ lib/beranda.dart
@@ -1,37 +1,248 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_wahyu/models/penjualan.dart';
+import 'package:flutter_application_wahyu/ui/inputpenjualan.dart';
 import './menuDrawer.dart' as menuDrawer;
+import 'produk.dart';
+import 'detail.dart';
+import 'detail_about.dart';
+import 'detail_produk.dart';
 
-class beranda extends StatefulWidget {
+class Beranda extends StatefulWidget {
   @override
-  _berandaState createState() => _berandaState();
+  _BerandaState createState() => _BerandaState();
 }
 
-class _berandaState extends State<beranda> {
+class _BerandaState extends State<Beranda> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text("Beranda Aplikasi"),
+      appBar: new AppBar(
+        backgroundColor: Colors.green,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text('Beranda'),
+        ]),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext) => produk()));
+            },
           ),
-          actions: [Icon(Icons.search)],
+          IconButton(
+            icon: Icon(Icons.notifications_active),
+            onPressed: () {
+              print('Click start');
+            },
+          )
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text("Wahyu Wastuguna"),
+              accountEmail: new Text("wahyu.wastuguna@undiksha.ac.id"),
+              currentAccountPicture: new GestureDetector(
+                onTap: () {},
+                child: new CircleAvatar(
+                  backgroundImage:
+                      new AssetImage('assets/appimage/formal.jpeg'),
+                ),
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: new NetworkImage(
+                        'https://www.shihoriobata.com/wp-content/uploads/2020/12/moon-and-clouds-aesthetic-background-phone2-576x1024.jpg?x45601'),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            new ListTile(
+              title: new Text('Notifications'),
+              trailing: new Icon(Icons.notifications_none),
+            ),
+            new ListTile(
+              title: new Text('Wishlist'),
+              trailing: new Icon(Icons.bookmark_border),
+            ),
+            new ListTile(
+              title: new Text('Akun'),
+              trailing: new Icon(Icons.verified_user),
+            ),
+            Divider(
+              height: 2,
+            ),
+            new ListTile(
+              title: new Text('About'),
+              trailing: new Icon(Icons.info),
+            ),
+            new ListTile(
+              title: new Text('Setting'),
+              trailing: new Icon(Icons.settings),
+            ),
+          ],
         ),
-        backgroundColor: Colors.black87,
-        drawer: menuDrawer.Menudrawer(),
-        body: Container(
-            child: ListView(children: <Widget>[
-          Column(children: <Widget>[
-            Card(
-                child: Column(
+      ),
+//seluruh boody dibungkus colomn
+      body: new ListView(
+        children: <Widget>[
+          Image(image: AssetImage("assets/appimage/pc gaming.jpg")),
+// setiap bagian pada body dipisahkan container
+          Container(
+            color: Colors.greenAccent[100],
+            padding: const EdgeInsets.all(10),
+            //untuk membuat tampilan secara horisontal maka digunakan row
+            child: Row(
               children: [
-                Image(image: AssetImage("assets/appimage/pc gaming.jpg")),
-                Text(
-                  "PC GAMING SET ",
-                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Rp 12.500.000',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        'Points 3.000',
+                        style: TextStyle(
+                          color: Colors.black87,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.local_offer,
+                  color: Colors.red[500],
+                ),
+                Text(' Up 15%'),
+              ],
+            ),
+          ),
+          //setiap bagian pada body dipisahkan container
+          Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: Theme.of(context).dividerColor)),
+            ),
+            //untuk membuat tampilan secara vertikal maka gunakan colomn
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //untuk membuat tampilan vertikal maka digunakan colomn
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.grid_on, color: Colors.red),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Kategori Barang",
+                        style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.share, color: Colors.red),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Bagikan",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.near_me, color: Colors.red),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Lokasi",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.call, color: Colors.red),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Telepon",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
                 )
               ],
-            ))
-          ]),
-        ])));
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: new BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.orange, Colors.blueAccent],
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Laptop & PC',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        'Diskon up to 15%',
+                        style: TextStyle(
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(' Edisi terbatas dan terbaru !'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,19 @@
 import 'package:flutter/material.dart';
-import './beranda.dart' as beranda;
-import './produk.dart' as produk;
+import "./beranda.dart" as beranda;
 
 void main() {
   runApp(new MaterialApp(
-    title: "tab bar",
+    title: "Penjualan Laptop & PC",
     home: new MyApp(),
   ));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  TabController controller; //widget untuk tabbar
-  @override
-  void initState() {
-    controller = new TabController(length: 2, vsync: this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: new TabBarView(
-        controller: controller,
-        children: <Widget>[
-          new beranda
-              .beranda(), // manggil kelas beranda inget import kelasnya dulu diatas
-          new produk.produk(),
-        ],
-      ),
-      bottomNavigationBar: new Material(
-        color: Colors.blueGrey,
-        child: new TabBar(controller: controller, tabs: <Widget>[
-          // widget tabs biar bisa ngisiin isi tabnya
-          new Tab(icon: new Icon(Icons.home)),
-          new Tab(
-            icon: new Icon(Icons.list),
-          ),
-        ]),
+    return new Scaffold(
+      body: Container(
+        child: beranda.Beranda(),
       ),
     );
   }
